18098b263aa0e885fef6e7ffcfcd234d4805bf97
siddharthmehta72 <siddharthmehta72@ymail.com>
2021-08-25 16:01:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "terraform-250821"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance1"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "${google_compute_network.vpc_network.self_link}"
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network1"
+  auto_create_subnetworks = "false"
+}
